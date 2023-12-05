4b90b5df30c97dff40a6f07a416fff35e669574d
“Nhat <nhat7203@gmail.com>
2023-12-05 00:47:15 -0500

    add unit test

---
+++ libcxx/test/libcxx/algorithms/const_callable.veriry.cpp
@@ -0,0 +1,16 @@
+#include <algorithm>
+
+struct ConstUncallable {
+  void operator()(int, int) const& = delete;
+  void operator()(int , int) & { return; }
+};
+
+struct NonConstUncallable {
+  void operator()(int, int) const&  { return; }
+  void operator()(int, int) & = delete;
+};
+
+void test() {
+  std::minmax({42, 0, -42}, ConstUncallable{});
+  //expected-error-re@*:* {{static_assert(__is_callable<_Compare const&, decltype(*__first), const _Tp&>::value, "The comparator has to be const-callable")}}
+}
