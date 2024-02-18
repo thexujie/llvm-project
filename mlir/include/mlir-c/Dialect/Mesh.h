7a6fad16da78f2844289423a7846e61582f2d026
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-02-18 22:49:24 +0100

    Add missing dialects to C API

---
+++ mlir/include/mlir-c/Dialect/Mesh.h
@@ -0,0 +1,25 @@
+//===-- mlir-c/Dialect/Mesh.h - C API for Mesh Dialect --------*- C -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM
+// Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef MLIR_C_DIALECT_MESH_H
+#define MLIR_C_DIALECT_MESH_H
+
+#include "mlir-c/IR.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+MLIR_DECLARE_CAPI_DIALECT_REGISTRATION(Mesh, mesh);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // MLIR_C_DIALECT_MESH_H
