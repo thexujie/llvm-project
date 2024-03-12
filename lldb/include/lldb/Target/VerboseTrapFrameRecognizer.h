f7f6d26dddab6feca7f978f250be1f5e5f058eb4
Michael Buch <michaelbuch12@gmail.com>
2024-03-12 11:05:39 +0000

    fixup! add license to header

--- lldb/include/lldb/Target/VerboseTrapFrameRecognizer.h
+++ lldb/include/lldb/Target/VerboseTrapFrameRecognizer.h
@@ -1,3 +1,11 @@
+//===-- VerboseTrapFrameRecognizer.h ----------------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
 #ifndef LLDB_TARGET_VERBOSETRAPFRAMERECOGNIZER_H
 #define LLDB_TARGET_VERBOSETRAPFRAMERECOGNIZER_H
 
