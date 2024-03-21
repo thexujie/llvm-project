f3e89fa28507c2e3ac9234a0add29a3c6742f1eb
Piyou Chen <piyou.chen@sifive.com>
2024-03-20 23:50:53 -0700

    More Sema testcase

--- clang/test/SemaCXX/attr-target-clones-riscv.cpp
+++ clang/test/SemaCXX/attr-target-clones-riscv.cpp
@@ -9,6 +9,15 @@ void __attribute__((target_clones("default", "mtune=sifive-u74"))) mtune() {}
 // expected-warning@+1 {{version list contains duplicate entries}}
 void __attribute__((target_clones("default", "arch=+c", "arch=+c"))) dupVersion() {}
 
+// expected-warning@+1 {{unsupported '' in the 'target_clones' attribute string; 'target_clones' attribute ignored}}
+void __attribute__((target_clones("default", ""))) emptyVersion() {}
+
+// expected-error@+1 {{'target_clones' multiversioning requires a default target}}
+void __attribute__((target_clones("arch=+c"))) withoutDefault() {}
+
+// expected-warning@+1 {{unsupported '+c' in the 'target_clones' attribute string; 'target_clones' attribute ignored}}
+void __attribute__((target_clones("default", "+c"))) invaildVersion() {}
+
 void lambda() {
   // expected-error@+1 {{attribute 'target_clones' multiversioned functions do not yet support lambdas}}
   auto x = []() __attribute__((target_clones("default"))){};
