10d4b9e505d5ad7476071153d2d13799f5b5cf1f
Haojian Wu <hokein.wu@gmail.com>
2024-03-25 15:10:51 +0100

    [clang] Implement a bitwise_copyable builtin type trait.
    This patch implements a `__is_bitwise_copyable` builtin in clang.
    
    The bitwise copyable types act as the trivially copyable types, but
    they support a wider range of types (e.g. classes with virtual methods) --
    their underlying types can be safely copied by `memcopy` or `memmove`,
    the clang compiler guarantees that both source and destination objects
    have the same *object* representations after the copy operation, and the
    lifetime of the destination object implicitly starts.
    
    A particular use case of this builtin is to clone an object via memcopy
    (without running the constructor):
    
    ```
    Message* clone(const Message* src, char* buffer, int size) {
      if constexpr __is_bitwise_copyable(Message) {
        // bitwise copy to buffer, and implicitly create objects at the buffer
        __builtin_memcpy(buffer, src, size);
        return std::launder(reinterpret_cast<Message*>(buffer));
      }
      // Fallback the operator new, which calls the constructor to start the lifetime.
      return new(buffer) Message(src);
    }
    ```
    
    Note that the definition of bitwise copyable is not tied to the Rule Of
    Five, so users of this builtin must guarantee that program semantic constraints
    are satisfied, e.g. no double resource deallocations.
    
    Context: https://discourse.llvm.org/t/extension-for-creating-objects-via-memcpy

--- clang/lib/Sema/SemaExprCXX.cpp
+++ clang/lib/Sema/SemaExprCXX.cpp
@@ -5109,6 +5109,8 @@ static bool CheckUnaryTypeTraitTypeCompleteness(Sema &S, TypeTrait UTT,
   case UTT_IsStandardLayout:
   case UTT_IsPOD:
   case UTT_IsLiteral:
+  // Clang extension:
+  case UTT_IsBitwiseCopyable:
   // By analogy, is_trivially_relocatable and is_trivially_equality_comparable
   // impose the same constraints.
   case UTT_IsTriviallyRelocatable:
@@ -5594,6 +5596,8 @@ static bool EvaluateUnaryTypeTrait(Sema &Self, TypeTrait UTT,
     return C.hasUniqueObjectRepresentations(T);
   case UTT_IsTriviallyRelocatable:
     return T.isTriviallyRelocatableType(C);
+  case UTT_IsBitwiseCopyable:
+    return T.isBitwiseCopyableType(C);
   case UTT_IsReferenceable:
     return T.isReferenceable();
   case UTT_CanPassInRegs:
