b92b55524dc10cc871b045acbfc4730f25c38a75
skc7 <Krishna.Sankisa@amd.com>
2024-04-09 17:17:03 +0530

    [AMDGPU] Move removeNoLdsKernelIdFromReachable from amdgpu-lower-module-lds pass to AMDGPUMemoryUtils

--- llvm/lib/Target/AMDGPU/Utils/AMDGPUMemoryUtils.cpp
+++ llvm/lib/Target/AMDGPU/Utils/AMDGPUMemoryUtils.cpp
@@ -241,6 +241,49 @@ LDSUsesInfoTy getTransitiveUsesOfLDS(CallGraph const &CG, Module &M) {
   return {std::move(direct_map_kernel), std::move(indirect_map_kernel)};
 }
 
+void removeFnAttrFromReachable(CallGraph &CG, Function *KernelRoot,
+                               StringRef FnAttr) {
+  KernelRoot->removeFnAttr(FnAttr);
+
+  SmallVector<Function *> Tmp({CG[KernelRoot]->getFunction()});
+  if (!Tmp.back())
+    return;
+
+  SmallPtrSet<Function *, 8> Visited;
+  bool SeenUnknownCall = false;
+
+  do {
+    Function *F = Tmp.pop_back_val();
+
+    for (auto &N : *CG[F]) {
+      if (!N.second)
+        continue;
+
+      Function *Callee = N.second->getFunction();
+      if (!Callee) {
+        if (!SeenUnknownCall) {
+          SeenUnknownCall = true;
+
+          // If we see any indirect calls, assume nothing about potential
+          // targets.
+          // TODO: This could be refined to possible LDS global users.
+          for (auto &N : *CG.getExternalCallingNode()) {
+            Function *PotentialCallee = N.second->getFunction();
+            if (!isKernelLDS(PotentialCallee))
+              PotentialCallee->removeFnAttr(FnAttr);
+          }
+
+          continue;
+        }
+      }
+
+      Callee->removeFnAttr(FnAttr);
+      if (Visited.insert(Callee).second)
+        Tmp.push_back(Callee);
+    }
+  } while (!Tmp.empty());
+}
+
 bool isReallyAClobber(const Value *Ptr, MemoryDef *Def, AAResults *AA) {
   Instruction *DefInst = Def->getMemoryInst();
 
