973b1d9974da27a5af49a42ad855ba35c5122f5e
skc7 <Krishna.Sankisa@amd.com>
2024-04-18 10:53:08 +0530

    [AMDGPU] Update removeFnAttrFromReachable

--- llvm/lib/Target/AMDGPU/Utils/AMDGPUMemoryUtils.cpp
+++ llvm/lib/Target/AMDGPU/Utils/AMDGPUMemoryUtils.cpp
@@ -245,21 +245,18 @@ void removeFnAttrFromReachable(CallGraph &CG, Function *KernelRoot,
                                StringRef FnAttr) {
   KernelRoot->removeFnAttr(FnAttr);
 
-  SmallVector<Function *> Tmp({CG[KernelRoot]->getFunction()});
-  if (!Tmp.back())
-    return;
-
+  SmallVector<Function *> WorkList({CG[KernelRoot]->getFunction()});
   SmallPtrSet<Function *, 8> Visited;
   bool SeenUnknownCall = false;
 
-  do {
-    Function *F = Tmp.pop_back_val();
+  while (!WorkList.empty()) {
+    Function *F = WorkList.pop_back_val();
 
-    for (auto &N : *CG[F]) {
-      if (!N.second)
+    for (auto &CallRecord : *CG[F]) {
+      if (!CallRecord.second)
         continue;
 
-      Function *Callee = N.second->getFunction();
+      Function *Callee = CallRecord.second->getFunction();
       if (!Callee) {
         if (!SeenUnknownCall) {
           SeenUnknownCall = true;
@@ -267,21 +264,21 @@ void removeFnAttrFromReachable(CallGraph &CG, Function *KernelRoot,
           // If we see any indirect calls, assume nothing about potential
           // targets.
           // TODO: This could be refined to possible LDS global users.
-          for (auto &N : *CG.getExternalCallingNode()) {
-            Function *PotentialCallee = N.second->getFunction();
+          for (auto &ExternalCallRecord : *CG.getExternalCallingNode()) {
+            Function *PotentialCallee =
+                ExternalCallRecord.second->getFunction();
+            assert(PotentialCallee);
             if (!isKernelLDS(PotentialCallee))
               PotentialCallee->removeFnAttr(FnAttr);
           }
-
-          continue;
         }
+      } else {
+        Callee->removeFnAttr(FnAttr);
+        if (Visited.insert(Callee).second)
+          WorkList.push_back(Callee);
       }
-
-      Callee->removeFnAttr(FnAttr);
-      if (Visited.insert(Callee).second)
-        Tmp.push_back(Callee);
     }
-  } while (!Tmp.empty());
+  }
 }
 
 bool isReallyAClobber(const Value *Ptr, MemoryDef *Def, AAResults *AA) {
