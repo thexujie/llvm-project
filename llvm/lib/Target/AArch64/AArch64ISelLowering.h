c1b1bd0418686b13b4437b658192671a27791954
Samuel Tebbs <samuel.tebbs@arm.com>
2024-04-24 14:58:06 +0100

    fixup: pass alloc size to ExpandZABuffer

--- llvm/lib/Target/AArch64/AArch64ISelLowering.h
+++ llvm/lib/Target/AArch64/AArch64ISelLowering.h
@@ -444,6 +444,7 @@ enum NodeType : unsigned {
   // SME
   RDSVL,
   REVD_MERGE_PASSTHRU,
+  EXPAND_ZA_BUFFER,
 
   // Asserts that a function argument (i32) is zero-extended to i8 by
   // the caller
